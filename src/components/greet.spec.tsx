2179386b2660a65a709cd5ecf6217b17a651ffa0
Nagendra Kumar S R <nagendrasr.webdesigner@gmailcom>
2025-07-31 22:26:57 +0530

    filename conventions

---
+++ src/components/greet.spec.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import { GreetTdd } from "./greetTdd";
+
+describe("Greet", () => {
+  it("renders correctly", () => {
+    render(<GreetTdd />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument;
+  });
+
+  describe("Nested", () => {
+    it("renders with a name", () => {
+      render(<GreetTdd name="Lorem" />);
+      const textElement = screen.getByText("Hello Lorem");
+      expect(textElement).toBeInTheDocument;
+    });
+  });
+});
