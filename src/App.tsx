035d8e63aff89711d9200c759314e9bf17b98c9e
Nagendra Kumar S R <nagendrasr.webdesigner@gmailcom>
2025-08-01 18:00:36 +0530

    getByRole

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import {Application} from "./components/application/application";
 
 function App() {
   return (
     <>
       <h1>Jest + React Testing library</h1>
       <p className="read-the-docs">Learn React Testing</p>
+      <Application />
     </>
   );
 }
