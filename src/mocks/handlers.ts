73aec421f79be279bc693c3605988066c0f56816
Nagendra Kumar S R <nagendrasr.webdesigner@gmailcom>
2025-08-04 21:53:29 +0530

    mock server worker error handling

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,12 +1,29 @@
+// // src/mocks/handlers.ts
 import { http, HttpResponse } from 'msw'
 
-// src/mocks/handlres.ts
 export const handlers = [
   http.get('https://jsonplaceholder.typicode.com/users', () => {
     return HttpResponse.json([
-        { name: 'John Doe' },
-        { name: 'Jane Smith' },
-        { name: 'Alice Johnson' }
-      ])
+      { name: 'John Doe' },
+      { name: 'Jane Smith' },
+      { name: 'Alice Johnson' }
+    ]);
   }),
-]
+];
+
+// prev version msw
+// import { rest } from 'msw'
+
+// export const handlers = [
+//   rest.get('https://jsonplaceholder.typicode.com/users', (req, res, ctx) => {
+//     return res(
+//       ctx.status(200),
+//       ctx.json([
+//         { name: 'John Doe' },
+//         { name: 'Jane Smith' },
+//         { name: 'Alice Johnson' }
+//       ])
+//     );
+//   })
+// ];
+
--- src/users/users.test.tsx
+++ src/users/users.test.tsx
@@ -1,6 +1,9 @@
 import { render, screen } from "@testing-library/react";
+// import { rest } from "msw";
+import { http, HttpResponse } from "msw";
 import { Users } from "./users";
-import "@testing-library/jest-dom"
+import "@testing-library/jest-dom";
+import { server } from "../mocks/server";
 
 describe("Users", () => {
   test("renders correctly", () => {
@@ -16,5 +19,20 @@ describe("Users", () => {
     expect(users[0]).toHaveTextContent("John Doe");
     expect(users[1]).toHaveTextContent("Jane Smith");
     expect(users[2]).toHaveTextContent("Alice Johnson");
+  });
+
+  test("renders error message on fetch failure", async () => {
+    server.use(
+      http.get("https://jsonplaceholder.typicode.com/users", () => {
+        return HttpResponse.json([
+          { name: "John Doe" },
+          { name: "Jane Smith" },
+          { name: "Alice Johnson" },
+        ]);
+      })
+    );
+    render(<Users />);
+    const errorMessage = await screen.findByText("Failed to fetch users");
+    expect(errorMessage).toBeInTheDocument();
   });
 });
