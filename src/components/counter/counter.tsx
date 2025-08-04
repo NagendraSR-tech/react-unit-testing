257239829c18a980fd7cf5138d99f2d03afceb88
Nagendra Kumar S R <nagendrasr.webdesigner@gmailcom>
2025-08-04 16:07:50 +0530

    test keyboard interations - type, tab, and add material UI

--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -2,11 +2,20 @@ import { useState } from 'react';
 
 export const Counter = () => {
     const [count, setCount] = useState(0);
+    const [amount, setAmount] = useState(1);
 
     return (
         <div>
             <h1>Count: {count}</h1>
             <button onClick={() => setCount(count + 1)}>Increment</button>
+            <label htmlFor="amount">Amount</label>
+            <input
+                type="number"
+                name="amount"
+                value={amount}
+                onChange={(e) => setAmount(parseInt(e.target.value))}
+            />
+            <button onClick={() => setCount(amount)}>Set</button>
         </div>
     );
 };
