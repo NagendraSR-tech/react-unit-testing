48342e6be4cd5b7bacd58f967c3ec9d1a27a7d96
Nagendra Kumar S R <nagendrasr.webdesigner@gmailcom>
2025-08-04 13:28:38 +0530

    findBy..., findAllBy... - when work with asyc code, returns promise

--- src/components/skills/skills.test.tsx
+++ src/components/skills/skills.test.tsx
@@ -27,4 +27,13 @@ describe("Skills", () => {
     const startLearningButton = screen.queryByRole("button", { name: /start learning/i });
     expect(startLearningButton).not.toBeInTheDocument();
   });
+
+  test("Start learning button is eventually displayed", async () => {
+    render(<Skills skills={skills} />);
+    const startLearningButton = await screen.findByRole("button", 
+      { name: /start learning/i },
+      { timeout: 2000 } // wait for up to 2 seconds
+    );
+    expect(startLearningButton).toBeInTheDocument();
+  });
 });
--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -1,10 +1,17 @@
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {  type SkillsProps as SkillsPropsType } from "./skills.types";
 
 export const Skills = (props: SkillsPropsType) => {
     const {skills} = props;
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoggedIn(true);
+        }, 1000);
+    }, []);
+    
     return (
         <div>
             <ul>
