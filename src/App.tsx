119cb7b6be41d90978a49fe99f3e9b720f98a5a2
Nagendra Kumar S R <nagendrasr.webdesigner@gmailcom>
2025-08-04 14:31:51 +0530

    pointer interactions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
-import {Application} from "./components/application/application";
+import { Application } from "./components/application/application";
+import { Counter } from "./components/counter/counter";
 import { Skills } from "./components/skills/skills";
 
 function App() {
   return (
     <>
-      <Skills skills={['JavaScript', 'React', 'CSS']} />
+      <Counter />
+      <Skills skills={["JavaScript", "React", "CSS"]} />
       <h1>Jest + React Testing library</h1>
       <p className="read-the-docs">Learn React Testing</p>
       <Application />
