4b5f9ec17c0058a5c7e3103f12e1e6377183bac4
Nagendra Kumar S R <nagendrasr.webdesigner@gmailcom>
2025-08-04 20:29:53 +0530

    remove comments - setupTests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,9 +1,5 @@
-// import { beforeAll, afterEach, afterAll } from 'vitest'
-// import { server } from './src/mocks/node'
-
 import '@testing-library/jest-dom'
 import { server } from './mocks/server'
-// import {server} from './mocks/server'
 
 beforeAll(() => server.listen())
 afterEach(() => server.resetHandlers())
